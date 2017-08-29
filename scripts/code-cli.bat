7281baf2a99d9ed251dd2647d65151c0124fb4b2
swanand joshi <31396992+meghjoshi123@users.noreply.github.com>
2017-08-29 08:29:49 +0530

    finish readme

--- scripts/code-cli.bat
+++ scripts/code-cli.bat
@@ -12,7 +12,56 @@ for /f "tokens=2 delims=:," %%a in ('findstr /R /C:"\"nameShort\":.*" product.js
 set NAMESHORT=%NAMESHORT: "=%
 set NAMESHORT=%NAMESHORT:"=%.exe
 set CODE=".build\electron\%NAMESHORT%"
+import java.io.*;
+import java.net.*;
 
+public class pract6
+{
+	public static void main (String args[])throws Exception
+	{
+		ServerSocket ss=new ServerSocket(8081);
+		while(true)
+			try
+			{
+				Socket s=ss.accept();
+				BufferedReader br=new BufferedReader(new InputStreamReader(s.getInputStream()));
+				PrintWriter pr=new PrintWriter(s.getOutputStream());
+				String str=null;
+				String str1=br.readLine();
+				System.out.println(str1);
+				while((str=br.readLine())!=null)
+				{
+					if(str.length()==0)
+						break;
+					System.out.println(str);
+				}
+				String str2=str1.substring(str1.indexOf("/")+1);
+				String str3=null;
+				try
+				{
+					str3=str2.substring(0,str2.indexOf("?"));
+				}
+				catch(Exception e)
+				{
+					str3=str2.substring(0,str2.indexOf(" "));
+				}
+					BufferedReader br1=new BufferedReader(new FileReader(str3));
+					while((str=br1.readLine())!=null)
+					{
+						System.out.println(str);
+						pr.println(str);
+					}
+					pr.println("</html>");
+					pr.close();
+					br.close();
+					br1.close();
+			}
+			catch(Exception e)
+			{
+				System.out.println(e);
+			}
+	}
+}
 for /f "tokens=2 delims=:," %%a in ('findstr /R /C:"\"electronVersion\":.*" package.json') do set DESIREDVERSION=%%~a
 set DESIREDVERSION=%DESIREDVERSION: "=%
 set DESIREDVERSION=v%DESIREDVERSION:"=%
